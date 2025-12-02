48deadc92fcee293349736d3cb3fb3045fddfbcb
Susan Gakii <susangakii71@gmail.com>
2025-12-02 16:01:35 +0300

    adds empty files/models to be used in class creation

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,15 @@
-import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-CONN = sqlite3.connect('company.db')
-CURSOR = CONN.cursor()
+engine = create_engine('sqlite:///dishcovery.db')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# import models
+from models.county import County
+from models.user import User
+from models.restaurant import Restaurant
+from models.dish import Dish
+from models.drink import Drink
+from models.review import Review
+from models.operating_hour import OperatingHour
